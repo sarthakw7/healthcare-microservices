10bc44f18d5ae4131e8eb9c5db4ed21ed5effb62
Sarthak S. Waghmare <sarthakw7@gmail.com>
2025-07-26 09:20:13 +0530

    Enable auth service access on 4005 and document login credentials

---
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -0,0 +1,39 @@
+package com.pm.authservice.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.WeakKeyException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key secretKey;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        try {
+            byte[] keyBytes = Base64.getDecoder().decode(secret.getBytes(
+                    StandardCharsets.UTF_8
+            ));
+            this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+        } catch (WeakKeyException e) {
+            throw new IllegalStateException("Failed to initialize JwtUtil. Check if 'jwt.secret' is base64 and valid length.", e);
+        }
+    }
+
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10hours
+                .signWith(secretKey)
+                .compact();
+    }
+}
