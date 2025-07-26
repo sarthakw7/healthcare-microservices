c9db8db021f6193964e40514259faccb34c04d3a
Sarthak S. Waghmare <sarthakw7@gmail.com>
2025-07-26 11:36:15 +0530

    Completed auth service with API gateway integration

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -1,13 +1,17 @@
 package com.pm.authservice.util;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.WeakKeyException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.security.SignatureException;
 import java.util.Base64;
 import java.util.Date;
 
@@ -35,5 +39,19 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10hours
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public void validateToken(String token){
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+        } catch (MalformedJwtException e) {
+            throw new JwtException("Malformed token", e);
+        } catch (SecurityException e) {
+            throw new JwtException("Invalid signature", e);
+        } catch (JwtException e) {
+            throw new JwtException("Invalid JWT", e);
+        }
     }
 }
